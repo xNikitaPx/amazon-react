fbf8435b436208ddd95920de485ea6f262080f17
Nikita <b.boynikone@gmail.com>
2025-12-20 19:24:00 +0200

    Move all pages to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router";
-import { HomePage } from "./pages/HomePage.jsx";
-import { CheckoutPage } from "./pages/CheckoutPage.jsx";
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
 import "./App.css";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   );
 }
